2c2aa4c8d6693d36c4e4f03bd55be9d2185b1e1f
Dinesh <dineshj6311@gmail.com>
2023-05-10 20:30:39 +0530

    Adding if statement

---
+++ src/if_statements/Practice_No1.java
@@ -0,0 +1,22 @@
+package if_statements;
+
+public class Practice_No1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=75;
+		int y=50;
+		
+		if (x>y)
+		{
+			System.out.println("Pass");
+		}
+		
+		if (x<y)
+		{
+			System.out.println("fail");
+		}
+	}
+
+}
---
+++ src/if_statements/Practice_no2.java
@@ -0,0 +1,10 @@
+package if_statements;
+
+public class Practice_no2 {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
