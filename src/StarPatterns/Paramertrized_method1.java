5ff1865ad7bb379d7f89c3444efa543d0e2d7b94
dinesh <dineshj6311@gmail.com>
2023-05-10 17:05:45 +0530

    New java program file added

---
+++ src/StarPatterns/Paramertrized_method1.java
@@ -0,0 +1,19 @@
+package StarPatterns;
+
+public class Paramertrized_method1 {
+	
+	int addition(int a, int b)
+	{
+	 int c=a+b;
+	 return c;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Paramertrized_method1 Ref1=new Paramertrized_method1();
+		
+		int X=Ref1.addition(20,30);
+		System.out.println(X);
+	}
+
+}
